3fed541944cd9e243a9f171f3f3c6356f8050461
DavidLlano2004 <juli.matias.2004@gmail.com>
2025-10-04 13:38:07 -0500

    Se suben cambios de back en register

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,12 +3,13 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { login, logout, register, verifyToken } from "../controllers/auth.controller.js";
 import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
+import { authRequired } from "../middlewares/validateToken.js";
 
 const router = Router();
 
 router.post("/login", validateSchema(loginSchema), login);
 
-router.post("/register", validateSchema(registerSchema), register);
+router.post("/register", validateSchema(registerSchema), authRequired , register);
 
 router.post("/logout", logout);
 
